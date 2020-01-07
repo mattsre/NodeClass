fa0b8565d22b2fe155177f06890cc49b2dd151cf
Matt Conway <mattconway55@gmail.com>
2020-01-06 20:32:20 -0500

    use Node HTTP lib, move off Express

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,13 +1,80 @@
-const express = require("express");
-const app = express();
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const port = 3000;
+const hostname = "127.0.0.1";
 
-app.use(
-  "/",
-  express.static("public", { index: false, extensions: ["html", "json"] })
-);
-app.get("*", (req, res) => res.redirect(301, "./index"));
+const todoHandler = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  fs.readFile(path.join(__dirname, "../public/todo.json"), (err, data) => {
+    if (err) {
+      res.writeHead(404, "Not Found");
+      res.write("404: File not found");
+      return res.end();
+    }
 
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
-);
+    res.statusCode = 200;
+    res.write(data);
+    return res.end();
+  });
+};
+
+const indexHandler = (req, res) => {
+  fs.readFile(path.join(__dirname, "../public/index.html"), (err, data) => {
+    if (err) {
+      res.writeHead(404, "Not Found");
+      res.write("404: File not found");
+      return res.end();
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write(data);
+    return res.end();
+  });
+};
+
+const readTodoHandler = (req, res) => {
+  fs.readFile(path.join(__dirname, "../public/read-todo.html"), (err, data) => {
+    if (err) {
+      res.writeHead(404, "Not Found");
+      res.write("404: File not found");
+      return res.end();
+    }
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write(data);
+    return res.end();
+  });
+};
+
+const notFoundHandler = (req, res) => {
+  res.writeHead(301, { Location: `http://${req.headers["host"]}/index` });
+  res.end();
+};
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/todo":
+      todoHandler(req, res);
+      break;
+
+    case "/index":
+      indexHandler(req, res);
+      break;
+
+    case "/read-todo":
+      readTodoHandler(req, res);
+      break;
+
+    default:
+      notFoundHandler(req, res);
+      break;
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server listening at http://${hostname}:${port}`);
+});
